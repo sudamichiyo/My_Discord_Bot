92a5d0885a29050871c7bae037ffd1c2b9e25cde
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-02-12 23:55:31 +0900

    ボットがHarukiInoueに返事を返す機能を追加

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+        # メッセージを書いた人がHaruki Inoue以外なら処理終了
+        if message.author.id != int(os.environ['USER_ID']):
+            return
+        channel = message.channel
+        await channel.send('Say hello!')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
