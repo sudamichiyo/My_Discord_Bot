2fd0379100e76661a088df7cab4acac5b56bb17a
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-02-12 23:08:39 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run(os.environ['DISCORD_TOKEN'])
