51087d994040c470e1f3ad79ca4c2fed09b97178
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-02-13 01:01:22 +0900

    起きたと言われたらおはようと返す機能を追加

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
-        # メッセージを書いた人がHaruki Inoue以外なら処理終了
+        # メッセージを書いた人がHaruki InoueのUSER_ID以外なら処理終了
         if message.author.id != int(os.environ['USER_ID']):
             return
-        channel = message.channel
-        await channel.send('Say hello!')
+        channel = message.channel 
+        topic = '起きた' #反応する文字列
+        text = 'おはよう' #返信する文字列
+        if message.content == topic:
+            await channel.send(text)
 
 intents = discord.Intents.default()
 intents.message_content = True
